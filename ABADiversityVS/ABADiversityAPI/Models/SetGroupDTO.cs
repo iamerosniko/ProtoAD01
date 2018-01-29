706b7820e842f8943f0eff5a5d7fb473585d9e56
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-29 11:01:53 -0500

    create entities for BTSS Connectivity

---
+++ ABADiversityVS/ABADiversityAPI/Models/SetGroupDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABADiversityAPI.Models
+{
+    public class SetGroupDTO
+    {
+        public string grp_id { get; set; }
+
+        public string grp_name { get; set; }
+
+        public string grp_desc { get; set; }
+    }
+}
