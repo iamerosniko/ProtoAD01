6215973a0258a4915a032e480b2327c0df4060da
Albert Rick G. Martires <martires.albert@gmail.com>
2018-02-05 15:41:28 -0500

    up
    - TopNav
    - SideNav
    - Survey Enhancements

--- ABADiversityVS/ABADiversity/clientapp/app/views/surveys/surveys.module.ts
+++ ABADiversityVS/ABADiversity/clientapp/app/views/surveys/surveys.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SurveysComponent } from './surveys.component';
+import { SideNavComponent } from '../side-nav/side-nav.component';
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SurveysComponent]
+  declarations: [SurveysComponent,SideNavComponent]
 })
 export class SurveysModule { }
