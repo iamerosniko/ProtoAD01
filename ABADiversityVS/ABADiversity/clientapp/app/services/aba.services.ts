4633beff9e874a1b14339f2cca180ef8cd23ec72
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-02-06 15:27:53 -0500

    linked created services to abaservices

--- ABADiversityVS/ABADiversity/clientapp/app/services/aba.services.ts
+++ ABADiversityVS/ABADiversity/clientapp/app/services/aba.services.ts
@@ -1,3 +1,13 @@
 export * from './clientapi.service';
 export * from './login.service';
 export * from './clientapi.settings';
+
+export * from './company-profiles.service';
+export * from './firm-demographics.service';
+export * from './firm-initiatives.service';
+export * from './firm-leaderships.service';
+export * from './high-compensated-partners.service';
+export * from './hours-reduced-lawyers.service';
+export * from './initiative-questions.service';
+export * from './joined-lawyers.service';
+export * from './left-lawyers.service';
