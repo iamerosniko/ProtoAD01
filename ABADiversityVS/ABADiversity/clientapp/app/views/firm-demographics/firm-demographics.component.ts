7bcbe68dd1c5847f0cca9947f0897a3aab6184b4
Albert Rick G. Martires <martires.albert@gmail.com>
2018-02-06 10:09:05 -0500

    up

---
+++ ABADiversityVS/ABADiversity/clientapp/app/views/firm-demographics/firm-demographics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firm-demographics',
+  templateUrl: './firm-demographics.component.html',
+  styleUrls: ['./firm-demographics.component.css']
+})
+export class FirmDemographicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ABADiversityVS/ABADiversity/clientapp/app/views/surveys/surveys.module.ts
+++ ABADiversityVS/ABADiversity/clientapp/app/views/surveys/surveys.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SurveysComponent } from './surveys.component';
 import { SideNavComponent } from '../side-nav/side-nav.component';
+import { SaveBarComponent } from '../save-bar/save-bar.component';
+import { CompanyProfileComponent } from '../company-profile/company-profile.component';
+import { FirmDemographicsComponent } from '../firm-demographics/firm-demographics.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SurveysComponent,SideNavComponent]
+  declarations: [SurveysComponent,SideNavComponent,SaveBarComponent,CompanyProfileComponent,FirmDemographicsComponent]
 })
 export class SurveysModule { }
