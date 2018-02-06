91c02763d9852c3e878b4bdbf7f4e5530ea38350
Albert Rick G. Martires <martires.albert@gmail.com>
2018-02-06 14:31:03 -0500

    up
    Firm Demographics

---
+++ ABADiversityVS/ABADiversity/clientapp/app/views/surveys/lawyers-left/lawyers-left.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lawyers-left',
+  templateUrl: './lawyers-left.component.html',
+  styleUrls: ['./lawyers-left.component.css']
+})
+export class LawyersLeftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ABADiversityVS/ABADiversity/clientapp/app/views/surveys/surveys.module.ts
+++ ABADiversityVS/ABADiversity/clientapp/app/views/surveys/surveys.module.ts
@@ -6,11 +6,14 @@ import { SideNavComponent } from './side-nav/side-nav.component';
 import { SaveBarComponent } from './save-bar/save-bar.component';
 import { CompanyProfileComponent } from './company-profile/company-profile.component';
 import { FirmDemographicsComponent } from './firm-demographics/firm-demographics.component';
+import { FirmLeadershipDemographicComponent } from './firm-leadership-demographic/firm-leadership-demographic.component';
+import { LawyersLeftComponent } from './lawyers-left/lawyers-left.component';
+import { LawyersJoinedComponent } from './lawyers-joined/lawyers-joined.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SurveysComponent,SideNavComponent,SaveBarComponent,CompanyProfileComponent,FirmDemographicsComponent]
+  declarations: [SurveysComponent,SideNavComponent,SaveBarComponent,CompanyProfileComponent,FirmDemographicsComponent, FirmLeadershipDemographicComponent, LawyersLeftComponent, LawyersJoinedComponent]
 })
 export class SurveysModule { }
