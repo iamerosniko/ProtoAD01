081da673b223092cdd45f99b63caeb94cf3c6b77
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-02-07 08:02:56 -0500

    updated the controller... removed unnecessary codes

--- ABADiversityVS/ABADiversity/clientapp/app/controllers/surveys-controller.ts
+++ ABADiversityVS/ABADiversity/clientapp/app/controllers/surveys-controller.ts
@@ -6,11 +6,6 @@ import { CompanyProfilesService,FirmDemographicsService,
     SizeCategoriesService
 } from '../services/aba.services';
 
-// import { CompanyProfiles,FirmDemographics,FirmInitiatives,
-//     FirmLeaderships,HighCompensatedPartners,HomegrownPartners,
-//     HoursReducedLawyers,InitiativeQuestions,JoinedLawyers,
-//     LeftLawyers,SizeCategories } from '../entities/aba-entities';
-
 import * as aba from '../entities/aba-entities';
 import { Surveys } from '../entities/surveys';
 export class SurveysController {
@@ -39,93 +34,82 @@ export class SurveysController {
         var hcpRes:aba.HighCompensatedPartners[];
         var fiRes:aba.FirmInitiatives[];
         var isComplete:boolean= false;
-        if(cpRes.CompanyProfileID>0||surveys.CompanyProfile.CompanyProfileID>0){
-            surveys.FIrmDemographics.forEach(async (element) => {
-                isNew 
-                ?
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID,
-                    fdRes.push(<aba.FirmDemographics>await this.firmDemographicsService.postFirmDemographics(element)))
-                : 
-                    fdRes.push(<aba.FirmDemographics>await this.firmDemographicsService.postFirmDemographics(element))                           
-                ;
-            });
+       
+        surveys.FIrmDemographics.forEach(async (element) => {
+            isNew 
+            ?
+                fdRes.push(<aba.FirmDemographics>await this.firmDemographicsService.postFirmDemographics(element))
+            : 
+                fdRes.push(<aba.FirmDemographics>await this.firmDemographicsService.postFirmDemographics(element))                           
+            ;
+        });
 
-            surveys.FirmLeaderships.forEach(async (element) => {
-                isNew
-                ?
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID,
-                    flRes.push(<aba.FirmLeaderships>await this.firmLeadershipsService.postFirmLeaderships(element)) )
-                : 
-                    flRes.push(<aba.FirmLeaderships>await this.firmLeadershipsService.postFirmLeaderships(element))                         
-                ;
-                       
-            });
+        surveys.FirmLeaderships.forEach(async (element) => {
+            isNew
+            ?
+                flRes.push(<aba.FirmLeaderships>await this.firmLeadershipsService.postFirmLeaderships(element)) 
+            : 
+                flRes.push(<aba.FirmLeaderships>await this.firmLeadershipsService.postFirmLeaderships(element))                         
+            ;
+                    
+        });
 
-            surveys.HomegrownPartners.forEach(async (element) => {
-                isNew 
-                ? 
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID,
-                    hgpRes.push(<aba.HomegrownPartners>await this.homeGrownPartnersService.postHomeGrownPartners(element))) 
-                : 
-                    hgpRes.push(<aba.HomegrownPartners>await this.homeGrownPartnersService.postHomeGrownPartners(element))                        
-                ;
-            });
+        surveys.HomegrownPartners.forEach(async (element) => {
+            isNew 
+            ? 
+                hgpRes.push(<aba.HomegrownPartners>await this.homeGrownPartnersService.postHomeGrownPartners(element))
+            : 
+                hgpRes.push(<aba.HomegrownPartners>await this.homeGrownPartnersService.postHomeGrownPartners(element))                        
+            ;
+        });
 
-            surveys.LeftLawyers.forEach(async (element) => {
-                isNew 
-                ? 
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID,
-                    llRes.push(<aba.LeftLawyers>await this.leftLawyersService.postLeftLawyers(element))) 
-                : 
-                    llRes.push(<aba.LeftLawyers>await this.leftLawyersService.postLeftLawyers(element))                        
-                ;
-            });
+        surveys.LeftLawyers.forEach(async (element) => {
+            isNew 
+            ? 
+                llRes.push(<aba.LeftLawyers>await this.leftLawyersService.postLeftLawyers(element))
+            : 
+                llRes.push(<aba.LeftLawyers>await this.leftLawyersService.postLeftLawyers(element))                        
+            ;
+        });
 
-            surveys.JoinedLawyers.forEach(async (element) => {
-                isNew 
-                ? 
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID,
-                    jlRes.push(<aba.JoinedLawyers>await this.joinedLawyersService.postJoinedLawyers(element)))
-                : 
-                    jlRes.push(<aba.JoinedLawyers>await this.joinedLawyersService.putJoinedLawyers(element))                        
-                ;
-            });
+        surveys.JoinedLawyers.forEach(async (element) => {
+            isNew 
+            ? 
+                jlRes.push(<aba.JoinedLawyers>await this.joinedLawyersService.postJoinedLawyers(element))
+            : 
+                jlRes.push(<aba.JoinedLawyers>await this.joinedLawyersService.putJoinedLawyers(element))                        
+            ;
+        });
 
-            surveys.HoursReducedLawyers.forEach(async (element) => {
-                isNew 
-                ?
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID, 
-                    hrlRes.push(<aba.HoursReducedLawyers>await this.hoursReducedLawyersService.postHoursReducedLawyers(element)))
-                : 
-                    hrlRes.push(<aba.HoursReducedLawyers>await this.hoursReducedLawyersService.putHoursReducedLawyers(element))
-                
-                ;
-            });
+        surveys.HoursReducedLawyers.forEach(async (element) => {
+            isNew 
+            ?
+                hrlRes.push(<aba.HoursReducedLawyers>await this.hoursReducedLawyersService.postHoursReducedLawyers(element))
+            : 
+                hrlRes.push(<aba.HoursReducedLawyers>await this.hoursReducedLawyersService.putHoursReducedLawyers(element))
+            ;
+        });
 
-            surveys.HighCompensatedPartners.forEach(async (element) => {
-                isNew 
-                ? 
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID, 
-                    hcpRes.push(<aba.HighCompensatedPartners>await this.highCompensatedPartnersService.postHighCompensatedPartners(element)))
-                : 
-                    hcpRes.push(<aba.HighCompensatedPartners>await this.highCompensatedPartnersService.putHighCompensatedPartners(element))
-                
-                ;
-            });
+        surveys.HighCompensatedPartners.forEach(async (element) => {
+            isNew 
+            ? 
+                hcpRes.push(<aba.HighCompensatedPartners>await this.highCompensatedPartnersService.postHighCompensatedPartners(element))
+            : 
+                hcpRes.push(<aba.HighCompensatedPartners>await this.highCompensatedPartnersService.putHighCompensatedPartners(element))
+            ;
+        });
 
-            surveys.FirmInitiatives.forEach(async (element) => {
-                isNew 
-                ? 
-                    (element.CompanyProfileID=await cpRes.CompanyProfileID,
-                    fiRes.push(<aba.FirmInitiatives>await this.firmInitiativesService.postFirmInitiatives(element)))
-                : 
-                    fiRes.push(<aba.FirmInitiatives>await this.firmInitiativesService.putFirmInitiatives(element))
-                ;
-            });
+        surveys.FirmInitiatives.forEach(async (element) => {
+            isNew 
+            ? 
+                fiRes.push(<aba.FirmInitiatives>await this.firmInitiativesService.postFirmInitiatives(element))
+            : 
+                fiRes.push(<aba.FirmInitiatives>await this.firmInitiativesService.putFirmInitiatives(element))
+            ;
+        });
 
-            isComplete=true;
-        }
-
+        isComplete=await true;
+    
         return new Promise<boolean>((res)=>res(isComplete));
     }
 }
