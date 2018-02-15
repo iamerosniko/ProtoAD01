5fc8f405da25e202aef639b97d6a6f5f10e4320e
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-02-15 09:28:00 -0500

    cleared all bugs

--- ABADiversityVS/ABADiversity/Startup.cs
+++ ABADiversityVS/ABADiversity/Startup.cs
@@ -44,10 +44,6 @@ namespace ABADiversity
         app.UseDeveloperExceptionPage();
         app.UseBrowserLink();
       }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-      }
 
       app.UseDefaultFiles();
 
@@ -55,12 +51,7 @@ namespace ABADiversity
 
       app.UseStaticFiles();
 
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-                  name: "default",
-                  template: "{controller=Home}/{action=Index}/{id?}");
-      });
+      app.UseMvc();
     }
   }
 }
