8d668c3a2463ff0e9b1a4dfd5d38c6d07ed7a0c3
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-08-22 13:29:48 -0400

    up

---
+++ ABAModelDiversity/API/DTO/RaceRoleValues.cs
@@ -0,0 +1,12 @@
+namespace API.DTO
+{
+  public class RaceRoleValues
+  {
+    public string Year { get; set; }
+    public string EquityPartners { get; set; }
+    public string NonEquityPartners { get; set; }
+    public string Associates { get; set; }
+    public string Counsel { get; set; }
+    public string OtherLawyers { get; set; }
+  }
+}
