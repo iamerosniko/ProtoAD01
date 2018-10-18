0d55b598102d78777571366b376c6e9fda6c738e
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-10-18 09:09:15 -0400

    up

--- API/Controllers/ReportsController.cs
+++ API/Controllers/ReportsController.cs
@@ -26,9 +26,20 @@ namespace API.Controllers
 
 
     private IEnumerable<CompanyProfiles> companyProfiles;
+
     private string[] Races = new string[] { "Asian", "Multiracial","Women", "Hispanic/Latino",
         "Native Hawaiian/Other Pacific Islander", "African American/Black(not Hispanic/Latino)",
         "Disabled","Alaska Native/American Indian","White", "Men","LGBT",
+      };
+
+    private string[] DiversityRankRace = new string[] { "Asian", "Multiracial", "Hispanic/Latino",
+        "Native Hawaiian/Other Pacific Islander", "African American/Black(not Hispanic/Latino)",
+        "Alaska Native/American Indian","White"
+      };
+
+    private string[] Minorities = new string[] { "Asian", "Multiracial", "Hispanic/Latino",
+        "Native Hawaiian/Other Pacific Islander", "African American/Black(not Hispanic/Latino)",
+        "Alaska Native/American Indian"
       };
 
     public ReportsController(ADContext context)
@@ -48,6 +59,99 @@ namespace API.Controllers
     }
 
     #region APIs
+    [HttpGet("GetDiversityRanking/")]
+    public void GetDiversityRanking()
+    {
+
+    }
+
+
+    [HttpGet("GetMinorities/{firmID}/{category}/{BaseSurvey}/{TopSurvey}")]
+    public List<RaceRoleValues> GetMinorities([FromRoute] Guid firmID, [FromRoute]int category, [FromRoute]Guid BaseSurvey, [FromRoute] Guid TopSurvey)
+    {
+      var raceRoleValues = GetReportRaceVSRole(firmID, category, BaseSurvey, TopSurvey);
+      List<RaceRoleValues> minorityRaceRoleValuesTemp = new List<RaceRoleValues>();
+      //filtering
+      double EquityPartners;
+      double OtherLawyers;
+      double Associates;
+      double Counsel;
+      double NonEquityPartners;
+      string year;
+
+      foreach (var minority in Minorities)
+      {
+        var raceRoleValue = raceRoleValues.Find(x => x.Race == minority);
+        if (raceRoleValue != null)
+        {
+          minorityRaceRoleValuesTemp.Add(raceRoleValue);
+        }
+      }
+
+      //remove the rating
+      foreach (var rrv in minorityRaceRoleValuesTemp)
+      {
+        rrv.MyRoleValues = rrv.MyRoleValues.Where(x => x.Year != "Rate").ToList();
+      }
+
+      //summation of every year
+      var numberOfRaceRoleValue = minorityRaceRoleValuesTemp.Count();
+      if (numberOfRaceRoleValue == 0)
+      {
+        return new List<RaceRoleValues>();
+      }
+
+      var numberOfYears = minorityRaceRoleValuesTemp[0].MyRoleValues.Count();
+
+      List<RaceRoleValues> minoritySummationList = new List<RaceRoleValues>();
+      RaceRoleValues minorityRaceRoleValue = new RaceRoleValues
+      {
+        Race = "Minorities",
+        MyRoleValues = new List<RoleValues>()
+      };
+
+
+      for (int i = 0; i < numberOfYears; i++)
+      {
+        EquityPartners = 0;
+        OtherLawyers = 0;
+        Associates = 0;
+        Counsel = 0;
+        NonEquityPartners = 0;
+        year = "";
+        for (int j = 0; j < numberOfRaceRoleValue; j++)
+        {
+          EquityPartners += ConvertToNumber(minorityRaceRoleValuesTemp[j].MyRoleValues[i].EquityPartners);
+          OtherLawyers += ConvertToNumber(minorityRaceRoleValuesTemp[j].MyRoleValues[i].OtherLawyers);
+          Associates += ConvertToNumber(minorityRaceRoleValuesTemp[j].MyRoleValues[i].Associates);
+          Counsel += ConvertToNumber(minorityRaceRoleValuesTemp[j].MyRoleValues[i].Counsel);
+          NonEquityPartners += ConvertToNumber(minorityRaceRoleValuesTemp[j].MyRoleValues[i].NonEquityPartners);
+          year = minorityRaceRoleValuesTemp[j].MyRoleValues[i].Year;
+        }
+
+        var minorityRoleValue = new RoleValues
+        {
+          Associates = Associates.ToString(),
+          Counsel = Counsel.ToString(),
+          EquityPartners = EquityPartners.ToString(),
+          NonEquityPartners = NonEquityPartners.ToString(),
+          OtherLawyers = NonEquityPartners.ToString(),
+          Year = year
+        };
+
+        minorityRoleValue.Total = Compute(minorityRoleValue);
+        minorityRaceRoleValue.MyRoleValues.Add(minorityRoleValue);
+      }
+      RoleValues rateRoleValue = new RoleValues();
+      rateRoleValue = getRate("Rate", minorityRaceRoleValue.MyRoleValues);
+      minorityRaceRoleValue.MyRoleValues.Add(rateRoleValue);
+      //main RaceVSRoles
+      minoritySummationList.Add(minorityRaceRoleValue);
+
+
+      return minoritySummationList;
+    }
+
     [HttpGet("GetReportRaceVSRole/{firmID}/{category}/{BaseSurvey}/{TopSurvey}")]
     public List<RaceRoleValues> GetReportRaceVSRole([FromRoute] Guid firmID, [FromRoute]int category, [FromRoute]Guid BaseSurvey, [FromRoute] Guid TopSurvey)
     {
@@ -66,26 +170,6 @@ namespace API.Controllers
       insideScopeOfCompanyProfiles = insideScopeOfCompanyProfiles.Where(x => x.CompanyProfileID != TopSurvey);
       //filter betweendates
       insideScopeOfCompanyProfiles = insideScopeOfCompanyProfiles.Where(x => x.Datecomp > baseCompanyProfiledate && x.Datecomp < topCompanyProfiledate).OrderBy(x => x.Datecomp);
-
-      //get firmdemographics
-      //var baseSurveyValue = _context.FirmDemographics.Where(x => x.CompanyProfileID == BaseSurvey).ToList();
-      //var topSurveyValue = _context.FirmDemographics.Where(x => x.CompanyProfileID == TopSurvey).ToList();
-
-      //List<List<FirmDemographics>> firmDemographics = new List<List<FirmDemographics>>();
-
-      //firmDemographics.Add(baseSurveyValue);
-
-      //foreach (var insideScopeOfCompanyProfile in insideScopeOfCompanyProfiles)
-      //{
-      //  var firmDemo = _context.FirmDemographics.Where(x => x.CompanyProfileID == insideScopeOfCompanyProfile.CompanyProfileID).ToList();
-      //  if (firmDemo != null)
-      //  {
-      //    firmDemographics.Add(firmDemo);
-      //  }
-      //}
-
-      //firmDemographics.Add(topSurveyValue);
-
 
       if (category == 0)
       {
@@ -140,19 +224,19 @@ namespace API.Controllers
 
     private RoleValues getRate(string race, List<RoleValues> roleValues)
     {
-      double baseAssociates = ConvertToInt(roleValues.First().Associates);
-      double baseCounsel = ConvertToInt(roleValues.First().Counsel);
-      double baseEquityPartners = ConvertToInt(roleValues.First().EquityPartners);
-      double baseNonEquityPartners = ConvertToInt(roleValues.First().NonEquityPartners);
-      double baseOtherLawyers = ConvertToInt(roleValues.First().OtherLawyers);
-      double baseTotal = ConvertToInt(roleValues.First().Total);
+      double baseAssociates = ConvertToNumber(roleValues.First().Associates);
+      double baseCounsel = ConvertToNumber(roleValues.First().Counsel);
+      double baseEquityPartners = ConvertToNumber(roleValues.First().EquityPartners);
+      double baseNonEquityPartners = ConvertToNumber(roleValues.First().NonEquityPartners);
+      double baseOtherLawyers = ConvertToNumber(roleValues.First().OtherLawyers);
+      double baseTotal = ConvertToNumber(roleValues.First().Total);
 
-      double topAssociates = ConvertToInt(roleValues.Last().Associates);
-      double topCounsel = ConvertToInt(roleValues.Last().Counsel);
-      double topEquityPartners = ConvertToInt(roleValues.Last().EquityPartners);
-      double topNonEquityPartners = ConvertToInt(roleValues.Last().NonEquityPartners);
-      double topOtherLawyers = ConvertToInt(roleValues.Last().OtherLawyers);
-      double topTotal = ConvertToInt(roleValues.Last().Total);
+      double topAssociates = ConvertToNumber(roleValues.Last().Associates);
+      double topCounsel = ConvertToNumber(roleValues.Last().Counsel);
+      double topEquityPartners = ConvertToNumber(roleValues.Last().EquityPartners);
+      double topNonEquityPartners = ConvertToNumber(roleValues.Last().NonEquityPartners);
+      double topOtherLawyers = ConvertToNumber(roleValues.Last().OtherLawyers);
+      double topTotal = ConvertToNumber(roleValues.Last().Total);
 
 
       double rateAssociates = Math.Round(((topAssociates - baseAssociates) / baseAssociates) * 100, 2);
@@ -187,16 +271,16 @@ namespace API.Controllers
     {
       return (
 
-        ConvertToInt(roleValue.EquityPartners) +
-        ConvertToInt(roleValue.NonEquityPartners) +
-        ConvertToInt(roleValue.OtherLawyers) +
-        ConvertToInt(roleValue.Associates) +
-        ConvertToInt(roleValue.Counsel)
+        ConvertToNumber(roleValue.EquityPartners) +
+        ConvertToNumber(roleValue.NonEquityPartners) +
+        ConvertToNumber(roleValue.OtherLawyers) +
+        ConvertToNumber(roleValue.Associates) +
+        ConvertToNumber(roleValue.Counsel)
         ).ToString();
 
     }
 
-    private double ConvertToInt(string stringValue)
+    private double ConvertToNumber(string stringValue)
     {
       try
       {
@@ -238,41 +322,41 @@ namespace API.Controllers
         for (int i = 0; i < racevalueofFD.MyRoleValues.Count(); i++)
         {
 
-          var assoc = ConvertToInt(racevalueofFD.MyRoleValues[i].Associates)
-            + ConvertToInt(racevalueofJL.MyRoleValues[i].Associates)
-            + ConvertToInt(racevalueofpap.MyRoleValues[i].Associates)
-            + ConvertToInt(racevalueofLL.MyRoleValues[i].Associates)
-            + ConvertToInt(racevalueofRHL.MyRoleValues[i].Associates);
+          var assoc = ConvertToNumber(racevalueofFD.MyRoleValues[i].Associates)
+            + ConvertToNumber(racevalueofJL.MyRoleValues[i].Associates)
+            + ConvertToNumber(racevalueofpap.MyRoleValues[i].Associates)
+            + ConvertToNumber(racevalueofLL.MyRoleValues[i].Associates)
+            + ConvertToNumber(racevalueofRHL.MyRoleValues[i].Associates);
 
-          var counsel = ConvertToInt(racevalueofFD.MyRoleValues[i].Counsel)
-            + ConvertToInt(racevalueofJL.MyRoleValues[i].Counsel)
-            + ConvertToInt(racevalueofpap.MyRoleValues[i].Counsel)
-            + ConvertToInt(racevalueofLL.MyRoleValues[i].Counsel)
-            + ConvertToInt(racevalueofRHL.MyRoleValues[i].Counsel);
+          var counsel = ConvertToNumber(racevalueofFD.MyRoleValues[i].Counsel)
+            + ConvertToNumber(racevalueofJL.MyRoleValues[i].Counsel)
+            + ConvertToNumber(racevalueofpap.MyRoleValues[i].Counsel)
+            + ConvertToNumber(racevalueofLL.MyRoleValues[i].Counsel)
+            + ConvertToNumber(racevalueofRHL.MyRoleValues[i].Counsel);
 
-          var ep = ConvertToInt(racevalueofFD.MyRoleValues[i].EquityPartners)
-            + ConvertToInt(racevalueofJL.MyRoleValues[i].EquityPartners)
-            + ConvertToInt(racevalueofpap.MyRoleValues[i].EquityPartners)
-            + ConvertToInt(racevalueofLL.MyRoleValues[i].EquityPartners)
-            + ConvertToInt(racevalueofRHL.MyRoleValues[i].EquityPartners);
+          var ep = ConvertToNumber(racevalueofFD.MyRoleValues[i].EquityPartners)
+            + ConvertToNumber(racevalueofJL.MyRoleValues[i].EquityPartners)
+            + ConvertToNumber(racevalueofpap.MyRoleValues[i].EquityPartners)
+            + ConvertToNumber(racevalueofLL.MyRoleValues[i].EquityPartners)
+            + ConvertToNumber(racevalueofRHL.MyRoleValues[i].EquityPartners);
 
-          var nep = ConvertToInt(racevalueofFD.MyRoleValues[i].NonEquityPartners)
-            + ConvertToInt(racevalueofJL.MyRoleValues[i].NonEquityPartners)
-            + ConvertToInt(racevalueofpap.MyRoleValues[i].NonEquityPartners)
-            + ConvertToInt(racevalueofLL.MyRoleValues[i].NonEquityPartners)
-            + ConvertToInt(racevalueofRHL.MyRoleValues[i].NonEquityPartners);
+          var nep = ConvertToNumber(racevalueofFD.MyRoleValues[i].NonEquityPartners)
+            + ConvertToNumber(racevalueofJL.MyRoleValues[i].NonEquityPartners)
+            + ConvertToNumber(racevalueofpap.MyRoleValues[i].NonEquityPartners)
+            + ConvertToNumber(racevalueofLL.MyRoleValues[i].NonEquityPartners)
+            + ConvertToNumber(racevalueofRHL.MyRoleValues[i].NonEquityPartners);
 
-          var ol = ConvertToInt(racevalueofFD.MyRoleValues[i].OtherLawyers)
-            + ConvertToInt(racevalueofJL.MyRoleValues[i].OtherLawyers)
-            + ConvertToInt(racevalueofpap.MyRoleValues[i].OtherLawyers)
-            + ConvertToInt(racevalueofLL.MyRoleValues[i].OtherLawyers)
-            + ConvertToInt(racevalueofRHL.MyRoleValues[i].OtherLawyers);
+          var ol = ConvertToNumber(racevalueofFD.MyRoleValues[i].OtherLawyers)
+            + ConvertToNumber(racevalueofJL.MyRoleValues[i].OtherLawyers)
+            + ConvertToNumber(racevalueofpap.MyRoleValues[i].OtherLawyers)
+            + ConvertToNumber(racevalueofLL.MyRoleValues[i].OtherLawyers)
+            + ConvertToNumber(racevalueofRHL.MyRoleValues[i].OtherLawyers);
 
-          var total = ConvertToInt(racevalueofFD.MyRoleValues[i].Total)
-            + ConvertToInt(racevalueofJL.MyRoleValues[i].Total)
-            + ConvertToInt(racevalueofpap.MyRoleValues[i].Total)
-            + ConvertToInt(racevalueofLL.MyRoleValues[i].Total)
-            + ConvertToInt(racevalueofRHL.MyRoleValues[i].Total);
+          var total = ConvertToNumber(racevalueofFD.MyRoleValues[i].Total)
+            + ConvertToNumber(racevalueofJL.MyRoleValues[i].Total)
+            + ConvertToNumber(racevalueofpap.MyRoleValues[i].Total)
+            + ConvertToNumber(racevalueofLL.MyRoleValues[i].Total)
+            + ConvertToNumber(racevalueofRHL.MyRoleValues[i].Total);
 
           var allYear = racevalueofFD.MyRoleValues[i].Year;
           rolevalues.Add(new RoleValues
---
+++ API/DTO/DiversityRank.cs
@@ -0,0 +1,10 @@
+namespace API.DTO
+{
+  public class DiversityRank
+  {
+    public int Rank { get; set; }
+    public string FirmName { get; set; }
+    public double Rate { get; set; }
+    public string RatePercentage { get; set; }
+  }
+}
