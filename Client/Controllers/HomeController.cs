759362c0c61300cc7ca7403dcab25b477cf8b572
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-10-25 09:05:37 -0400

    up

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -1,37 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Client.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Client.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      if (HttpContext.User.Identity.Name == "" || HttpContext.User.Identity.Name == null)
+      {
+        return Redirect("~/SignIn");
+      }
+      return View();
     }
+
+    [Route("signin")]
+    public IActionResult SignIn()
+    {
+      return Challenge(new AuthenticationProperties { RedirectUri = "/" });
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
--- Client/Startup.cs
+++ Client/Startup.cs
@@ -1,50 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Newtonsoft.Json.Serialization;
 
 namespace Client
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      Configuration = configuration;
     }
+    public static IConfiguration Configuration { get; private set; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<MvcOptions>(options =>
+      {
+        options.Filters.Add(new CorsAuthorizationFilterFactory("CORS"));
+      });
+
+      services.AddAuthentication(sharedOptions =>
+      {
+        sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        sharedOptions.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+      })
+      .AddOpenIdConnect(options =>
+      {
+        options.Authority = Configuration["ADAUTH:Authority"];
+        options.ClientId = Configuration["ADAUTH:ClientID"];
+        options.ResponseType = OpenIdConnectResponseType.IdToken;
+        options.CallbackPath = Configuration["ADAUTH:Callback"];
+        //options.CallbackPath = "/.auth/login/aad/callback";
+      })
+      .AddCookie();
+
+      //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+      services.AddMvc()
+      .AddJsonOptions(o =>
+      {
+        if (o.SerializerSettings.ContractResolver != null)
+        {
+          var castedResolver = o.SerializerSettings.ContractResolver
+                        as DefaultContractResolver;
+          castedResolver.NamingStrategy = null;
+        }
+      });
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CORS",
+        corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
+
+      });
+
+      services.AddSession();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      app.UseAuthentication();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        //app.UseExceptionHandler("/Home/Error");
+        //app.UseHsts();
+      }
+
+      //app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseSession();
+      app.UseCors("CORS");
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
 }
